31996650814fb7079ddcb7f59e278df32195d955
leberhard5869 <58434277+leberhard5869@users.noreply.github.com>
2021-02-01 16:24:59 -0500

    Impement search state

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from "./src/screens/SearchScreen";
 
 const navigator = createStackNavigator({
@@ -7,7 +8,7 @@ const navigator = createStackNavigator({
 }, {
   initialRouteName: 'Search',
   defaultNavigationOptions: {
-    title: 'BusinessSearch'
+    title: 'Business Search'
   }
 });
 
