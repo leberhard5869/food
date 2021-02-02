337562a2b57917c65c1939a13285faaee188638a
leberhard5869 <58434277+leberhard5869@users.noreply.github.com>
2021-02-02 11:45:07 -0500

    Styling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,19 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'toronto'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something wrong');
-    }
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    });
   };
-  
+
   return (
     <View>
       <SearchBar
@@ -32,13 +23,16 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ScrollView>
+        <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"  />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"  />
+        <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender"  />
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
 
